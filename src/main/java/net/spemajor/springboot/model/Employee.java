3398618ef252c6fb5d089615f2fff694a1528b0d
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-12-01 23:16:42 +0530

    Connected with MySQL Database

---
+++ src/main/java/net/spemajor/springboot/model/Employee.java
@@ -0,0 +1,14 @@
+// package net.spemajor.springboot.model;
+
+// import lombok.Setter;
+// import lombok.Getter;
+
+// public class Employee 
+// {
+//     private long id;
+//     private String firstName;
+//     private String lastName;
+//     private String emailID;    
+
+
+// }
