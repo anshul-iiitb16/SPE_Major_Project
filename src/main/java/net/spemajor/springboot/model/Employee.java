e079a1ee553ba1aacb814c2db6d93b6aa6598cf8
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-12-02 21:34:40 +0530

    Added Target

--- src/main/java/net/spemajor/springboot/model/Employee.java
+++ src/main/java/net/spemajor/springboot/model/Employee.java
@@ -1,14 +1,28 @@
-// package net.spemajor.springboot.model;
+package net.spemajor.springboot.model;
 
-// import lombok.Setter;
-// import lombok.Getter;
+import lombok.Setter;
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-// public class Employee 
-// {
-//     private long id;
-//     private String firstName;
-//     private String lastName;
-//     private String emailID;    
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
 
-
-// }
+public class Employee 
+{
+    // Setting Id as a P.K.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailID;    
+}
