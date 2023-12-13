d410aded383b54866b0190219d40fcc2d7e22180
anshul-iiitb16 <Anshul.Jindal@iiitb.org>
2023-12-13 17:59:07 +0530

    Updated packages

--- react-hooks-frontend/src/App.test.js
+++ react-hooks-frontend/src/App.test.js
@@ -1,5 +1,14 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 import { render, screen } from '@testing-library/react';
-import TestRenderer from "react-test-renderer";
+import TestRenderer from 'react-test-renderer';
 import App from './App';
 
 it("Renders the App correctly", () => {
